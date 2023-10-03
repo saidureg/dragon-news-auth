099c8431d3d2830bd9fc32bc2baf5a748d0ccdca
saidureg <saidureg@gmail.com>
2023-10-03 16:11:56 +0600

    date time formatting using moment.js

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,12 +1,25 @@
 import Header from "../shared/Header/Header";
+import LeftSideNavbar from "../shared/LeftSideNavbar/LeftSideNavbar";
 import Navbar from "../shared/Navbar/Navbar";
+import RightSideNavbar from "../shared/RightSideNavbar/RightSideNavbar";
 
 const Home = () => {
   return (
     <div>
       <Header></Header>
       <Navbar></Navbar>
-      <h3>Home page</h3>
+
+      <div className="grid grid-cols-1 md:grid-cols-4 gap-6">
+        <div>
+          <LeftSideNavbar></LeftSideNavbar>
+        </div>
+        <div className="md:col-span-2">
+          <h3 className="text-6xl">Home content coming soon....</h3>
+        </div>
+        <div>
+          <RightSideNavbar></RightSideNavbar>
+        </div>
+      </div>
     </div>
   );
 };
--- src/pages/shared/Header/Header.jsx
+++ src/pages/shared/Header/Header.jsx
@@ -1,7 +1,15 @@
+import logo from "../../../assets/logo.png";
+import moment from "moment";
 const Header = () => {
   return (
     <div>
-      <h3 className="text-3xl">Header</h3>
+      <img src={logo} alt="logo" />
+      <p className="text-lg text-[#706F6F]">
+        Journalism Without Fear or Favour
+      </p>
+      <p className="text-xl font-medium text-[#403F3F]">
+        {moment().format(`dddd, MMMM D YYYY`)}
+      </p>
     </div>
   );
 };
