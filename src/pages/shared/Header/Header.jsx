33af0bd6ea004628546c0c06f874593e44b3cd0c
saidureg <saidureg@gmail.com>
2023-10-03 19:28:12 +0600

    Right navigation and info with icons add

--- src/pages/shared/Header/Header.jsx
+++ src/pages/shared/Header/Header.jsx
@@ -2,7 +2,7 @@ import logo from "../../../assets/logo.png";
 import moment from "moment";
 const Header = () => {
   return (
-    <div className="text-center">
+    <div className="text-center space-y-3 my-6">
       <img className="mx-auto" src={logo} alt="logo" />
       <p className="text-lg text-[#706F6F]">
         Journalism Without Fear or Favour
--- src/pages/shared/RightSideNavbar/RightSideNavbar.jsx
+++ src/pages/shared/RightSideNavbar/RightSideNavbar.jsx
@@ -1,7 +1,58 @@
+import {
+  FaGithub,
+  FaFacebook,
+  FaGoogle,
+  FaTwitter,
+  FaInstagram,
+} from "react-icons/fa";
+import qZone1 from "../../../assets/qZone1.png";
+import qZone2 from "../../../assets/qZone2.png";
+import qZone3 from "../../../assets/qZone3.png";
+
 const RightSideNavbar = () => {
   return (
     <div>
-      <h3 className="text-2xl">RightSideNavbar</h3>
+      <div>
+        <h3 className="text-2xl p-4">Login With</h3>
+        <div className="space-y-2 p-4">
+          <button className="btn btn-outline w-full">
+            <FaGoogle></FaGoogle> Login with Google
+          </button>
+          <button className="btn btn-outline w-full">
+            <FaGithub></FaGithub> Login with Google
+          </button>
+        </div>
+      </div>
+      {/* Find us on */}
+      <div>
+        <h3 className="text-2xl p-4">Find Us On</h3>
+        <div className="px-4">
+          <a
+            className="flex items-center gap-2 border rounded-t-lg py-2 px-3"
+            href=""
+          >
+            <FaFacebook></FaFacebook> Facebook
+          </a>
+          <a className="flex items-center gap-2 border-x py-2 px-3" href="">
+            <FaTwitter></FaTwitter> Twitter
+          </a>
+          <a
+            className="flex items-center gap-2 border rounded-b-lg py-2 px-3"
+            href=""
+          >
+            <FaInstagram></FaInstagram> Instagram
+          </a>
+        </div>
+      </div>
+      {/* Q-zone */}
+      <div className="mt-4 bg-[#F3F3F3] rounded p-4">
+        <h3 className="text-2xl mb-2">Q-Zone</h3>
+        <div className="space-y-2 ">
+          <img src={qZone1} alt="" />
+          <img src={qZone2} alt="" />
+          <img src={qZone3} alt="" />
+        </div>
+      </div>
     </div>
   );
 };
